ff3f0d2558e464bb7c3d39554dc050ccff4eb541
domaaron <daaronjhay@gmx.at>
2025-09-09 10:23:51 +0200

    Add project files.

---
+++ Media Ratings Platform/IMediaEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform
+{
+    internal interface IMediaEntry
+    {
+        string Title { get; set; }
+        string Description { get; set; }
+        string MediaType { get; set; }
+        DateTime ReleaseYear { get; set; }
+        string Genres { get; set; }
+        int AgeRestriction { get; set; }
+    }
+}
