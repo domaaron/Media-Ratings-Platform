eca62e4eaa1efb792a504cc6dda14314ecb50b30
Aaron Domingo <daaronjhay@gmx.at>
2025-09-13 14:17:55 +0200

    updated according to abstract class MediaEntry

--- Media Ratings Platform/models/Movie.cs
+++ Media Ratings Platform/models/Movie.cs
@@ -6,49 +6,10 @@ using System.Threading.Tasks;
 
 namespace Media_Ratings_Platform.models
 {
-    public class Movie : IMediaEntry
+    public class Movie : MediaEntry
     {
-        private string _title;
-        private string _description;
-        private string _mediaType;
-        private DateTime _releaseYear;
-        private Genres _genres;
-        private int _ageRestriction;
-
-        public string Title
-        {
-            get => _title;
-            set => _title = value;
-        }
-
-        public string Description
-        {
-            get => _description; 
-            set => _description = value;
-        }
-
-        public string MediaType
-        {
-            get => _mediaType;
-            set => _mediaType = value;
-        }
-
-        public DateTime ReleaseYear
-        {
-            get => _releaseYear;
-            set => _releaseYear = value;
-        }
-
-        public Genres Genres
-        {
-            get => _genres;
-            set => _genres = value;
-        }
-
-        public int AgeRestriction
-        {
-            get => _ageRestriction;
-            set => _ageRestriction = value;
-        }
+        public override MediaType MediaType => MediaType.Movie;
+        public Movie(string title, string description, int releaseYear, List<Genres> genres, int ageRestriction)
+            : base(title, description, releaseYear, genres, ageRestriction) { }
     }
 }
