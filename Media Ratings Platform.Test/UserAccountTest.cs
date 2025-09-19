edf13a55604f7aa41057d227f062214da50a05c6
Aaron Domingo <daaronjhay@gmx.at>
2025-09-19 15:26:32 +0200

    created unit tests

---
+++ Media Ratings Platform.Test/UserAccountTest.cs
@@ -0,0 +1,176 @@
+using Media_Ratings_Platform.models;
+
+namespace Media_Ratings_Platform.Test
+{
+    public class UserAccountTest
+    {
+        [Fact]
+        public void CreateUserSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            Assert.True(user != null);
+        }
+
+        [Fact]
+        public void AddMediaEntryTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            user.AddMediaEntry(movie);
+
+            Assert.Contains(movie, user.MediaEntries);
+            Assert.Equal(1, user.TotalMediaEntries());
+        }
+
+        [Fact]
+        public void RemoveMediaEntrySuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            user.AddMediaEntry(movie);
+            user.RemoveMediaEntry(movie);
+
+            Assert.DoesNotContain(movie, user.MediaEntries);
+            Assert.Equal(0, user.TotalMediaEntries());
+        }
+
+        [Fact]
+        public void AddFavoriteSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            user.AddFavorite(movie);
+
+            Assert.Contains(movie, user.Favorites);
+            Assert.Equal(1, user.TotalFavorites());
+        }
+
+        [Fact]
+        public void RemoveFavoriteSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            user.AddFavorite(movie);
+            user.RemoveFavorite(movie);
+
+            Assert.DoesNotContain(movie, user.Favorites);
+            Assert.Equal(0, user.TotalFavorites());
+        }
+
+        [Fact]
+        public void AddRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Can recommend");
+            user.AddRating(rating);
+
+            Assert.Contains(user.Ratings, r => r.MediaEntry == movie);
+            Assert.Equal(1, user.TotalRatings());
+        }
+
+        [Fact]
+        public void RemoveRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Can recommend");
+            user.AddRating(rating);
+            user.RemoveRating(rating);
+
+            Assert.DoesNotContain(user.Ratings, r => r.MediaEntry == movie);
+            Assert.Equal(0, user.TotalRatings());
+        }
+
+        [Fact]
+        public void LikeRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUser = new UserAccount("Alice", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, otherUser, 5, "Can recommend");
+            otherUser.AddRating(rating);
+
+            Assert.True(user.LikeRating(rating));
+        }
+
+        [Fact]
+        public void LikeOwnRatingFailTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Can recommend");
+            user.AddRating(rating);
+
+            Assert.False(user.LikeRating(rating));
+        }
+
+        [Fact]
+        public void LikeSameRatingTwiceFailTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUser = new UserAccount("Alice", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, otherUser, 5, "Can recommend");
+            otherUser.AddRating(rating);
+
+            user.LikeRating(rating);
+
+            Assert.False(user.LikeRating(rating));
+        }
+    }
+}
---
+++ Media Ratings Platform.Test/UserRatingTest.cs
@@ -0,0 +1,139 @@
+﻿using Media_Ratings_Platform.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.Test
+{
+    public class UserRatingTest
+    {
+        [Fact]
+        public void ConfirmRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, null);
+            rating.Confirm();
+
+            Assert.True(rating.IsConfirmed);
+        }
+
+        [Fact]
+        public void EditRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, null);
+            rating.EditRating(user, 4, "Nice");
+
+            Assert.Equal(4, rating.StarValue);
+            Assert.False(rating.IsConfirmed);
+        }
+
+        [Fact]
+        public void EditRatingFailTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUser = new UserAccount("Alice", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, null);
+
+            Assert.Throws<UnauthorizedAccessException>(() => rating.EditRating(otherUser, 4, "Nice"));
+        }
+
+        [Fact]
+        public void DeleteRatingSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUser = new UserAccount("Alice", "test");
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Nice");
+            rating.DeleteRating();
+
+            Assert.Equal(0, rating.StarValue);
+            Assert.Null(rating.Comment);
+        }
+
+        [Fact]
+        public void AddLikeSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUserId = Guid.NewGuid();
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Nice");
+            rating.AddLike(otherUserId);
+
+            Assert.Contains(otherUserId, rating.LikedBy);
+        }
+
+        [Fact]
+        public void AddLikeTwiceFailTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUserId = Guid.NewGuid();
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Nice");
+            rating.AddLike(otherUserId);
+            rating.AddLike(otherUserId);
+
+            Assert.Single(rating.LikedBy);
+        }
+
+        [Fact]
+        public void RemoveLikeSuccessTest()
+        {
+            var user = new UserAccount("Max", "test");
+            var otherUserId = Guid.NewGuid();
+            var movie = new Movie(
+                "Cars",
+                "It's about cars.",
+                2006,
+                new List<Genres> { Genres.Animation, Genres.Comedy },
+                6
+            );
+            var rating = new UserRating(movie, user, 5, "Nice");
+            rating.AddLike(otherUserId);
+            rating.RemoveLike(otherUserId);
+
+            Assert.DoesNotContain(otherUserId, rating.LikedBy);
+        }
+    }
+}
