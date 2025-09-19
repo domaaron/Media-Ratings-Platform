65a58cef3745db14b3b2619bff6571429f08fbe6
Aaron Domingo <daaronjhay@gmx.at>
2025-09-19 15:21:40 +0200

    added Guid to constructor

--- Media Ratings Platform/models/Series.cs
+++ Media Ratings Platform/models/Series.cs
@@ -10,6 +10,6 @@ namespace Media_Ratings_Platform.models
     {
         public override MediaType MediaType => MediaType.Series;
         public Series(string title, string description, int releaseYear, List<Genres> genres, int ageRestriction)
-            : base(title, description, releaseYear, genres, ageRestriction) { }
+            : base(Guid.NewGuid(), title, description, releaseYear, genres, ageRestriction) { }
     }
 }
