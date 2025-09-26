6d7553e6d129b17c0b38321c225f3aaeb722a223
Aaron Domingo <daaronjhay@gmx.at>
2025-09-26 11:27:43 +0200

    moved methods to the services

--- MediaRating.Domain/models/UserAccount.cs
+++ MediaRating.Domain/models/UserAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediaRatings.Domain.services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,132 +25,19 @@ namespace MediaRatings.Domain
         {
             Username = username;
             Password = password;
+
+            MediaManager = new MediaManager();
+            FavoritesManager = new FavoritesManager();
+            RatingManager = new RatingManager(this);
         }
 
         public Guid UserId { get; private set; } = Guid.NewGuid();
         public string Username { get; set; }
         public string Password { get; set; }
 
-        // media created by own user
-        public List<IMediaEntry> MediaEntries { get; private set; } = new List<IMediaEntry> { };
-        // ratings created by own user
-        public List<UserRating> Ratings { get; private set; } = new List<UserRating> { };
-        // favourites, no duplicate values
-        public HashSet<IMediaEntry> Favorites { get; private set; } = new HashSet<IMediaEntry>();
-
-        /* 
-         * ======================================
-         * media management
-         * ======================================
-        */
-        public void AddMediaEntry(IMediaEntry mediaEntry)
-        {
-            if (!MediaEntries.Contains(mediaEntry))
-            {
-                MediaEntries.Add(mediaEntry);
-            }
-        }
-
-        public void RemoveMediaEntry(IMediaEntry mediaEntry)
-        {
-            MediaEntries.Remove(mediaEntry);
-        }
-
-        public void UpdateMediaEntry(IMediaEntry oldEntry, IMediaEntry newEntry)
-        {
-            int index = MediaEntries.IndexOf(oldEntry);
-            if (index >= 0)
-            {
-                MediaEntries[index] = newEntry;
-            }
-        }
-
-        /* 
-         * ======================================
-         * favourites
-         * ======================================
-        */
-        public void AddFavorite(IMediaEntry mediaEntry)
-        {
-            Favorites.Add(mediaEntry);
-        }
-
-        public void RemoveFavorite(IMediaEntry mediaEntry)
-        {
-            Favorites.Remove(mediaEntry);
-        }
-
-        /* 
-         * ======================================
-         * ratings
-         * ======================================
-        */
-        public void AddRating(UserRating rating)
-        {
-            if (rating.StarValue < 1 || rating.StarValue > 5)
-            {
-                throw new ArgumentOutOfRangeException("Invalid rating: Stars must be between 1 and 5.");
-            }
-
-            Ratings.Add(rating);
-        }
-
-        public void RemoveRating(UserRating rating)
-        {
-            Ratings.Remove(rating);
-        }
-
-        public bool LikeRating(UserRating rating)
-        {
-            // cannot like own rating
-            if (rating.User == this)
-            {
-                return false;
-            }
-
-            // cannot like twice the same rating
-            if (rating.LikedBy.Contains(this.UserId))
-            {
-                return false;
-            }
-
-            rating.LikedBy.Add(this.UserId);
-            return true;
-        }
-
-        public IEnumerable<UserRating> GetRatingHistory()
-        {
-            return Ratings.OrderByDescending(r => r.RatingTimestamp);
-        }
-
-        /* 
-         * ======================================
-         * statistics
-         * ======================================
-        */
-        public double AverageRatingGiven()
-        {
-            if (!Ratings.Any())
-            {
-                return 0;
-            }
-
-            return Ratings.Average(r => r.StarValue);
-        }
-
-        public int TotalFavorites()
-        {
-            return Favorites.Count;
-        }
-
-        public int TotalMediaEntries()
-        {
-            return MediaEntries.Count;
-        }
-
-        public int TotalRatings()
-        {
-            return Ratings.Count;
-        }
+        // services
+        public MediaManager MediaManager { get; private set; }
+        public FavoritesManager FavoritesManager { get; private set; }
+        public RatingManager RatingManager { get; private set; }
     }
 }
