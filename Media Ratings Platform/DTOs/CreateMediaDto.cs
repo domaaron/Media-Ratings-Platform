f87682fd9012d1cfe43e001fe592a67e31cac770
Aaron Domingo <daaronjhay@gmx.at>
2025-10-12 10:54:57 +0200

    created for POST and PUT endpoints

---
+++ Media Ratings Platform/DTOs/CreateMediaDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.DTOs
+{
+    public class CreateMediaDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public List<string> Genres { get; set; } = new();
+        public string MediaType { get; set; } = string.Empty;
+        public int AgeRestriction { get; set; }
+    }
+}
---
+++ Media Ratings Platform/DTOs/UpdateMediaDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.DTOs
+{
+    public class UpdateMediaDto
+    {
+        public int MediaId { get; set; }
+        public int CreatedBy { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string MediaType { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public List<string> Genres { get; set; } = new();
+        public int AgeRestriction { get; set; }
+    }
+}
