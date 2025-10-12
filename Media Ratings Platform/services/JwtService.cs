9e5df8389b01bb6437c7b1668b4d5211e8021ace
Aaron Domingo <daaronjhay@gmx.at>
2025-10-12 11:12:01 +0200

    added new class for generating and validating tokens

---
+++ Media Ratings Platform/services/JwtService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.services
+{
+    public class JwtService
+    {
+        private readonly string _secret;
+
+        public JwtService(string secret)
+        {
+            _secret = secret;
+        }
+        public string GenerateToken(string username)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                },
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string? ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secret);
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+
+                Console.WriteLine("Token claims: ");
+                foreach (var c in jwtToken.Claims)
+                {
+                    Console.WriteLine($"{c.Type} => {c.Value}");
+                }
+
+                var username = jwtToken.Claims.First(x => x.Type == "username").Value;
+                return username;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("JWT validation failed: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
