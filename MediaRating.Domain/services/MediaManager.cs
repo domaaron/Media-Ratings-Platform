2af1efe1dbe43a362306c40f59bf8182e9d95fc0
Aaron Domingo <daaronjhay@gmx.at>
2025-10-12 10:54:18 +0200

    adjusted class to changes in IMediaManager

--- MediaRating.Domain/services/MediaManager.cs
+++ MediaRating.Domain/services/MediaManager.cs
@@ -1,4 +1,5 @@
-﻿using MediaRatings.Domain.interfaces;
+﻿using Media_Ratings_Platform.DTOs;
+using MediaRatings.Domain.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace MediaRatings.Domain.services
     {
         // media created by own user
         private readonly List<IMediaEntry> _mediaEntries = new();
+
+        public IMediaEntry? GetMediaById(int id)
+        {
+            return _mediaEntries.OfType<MediaEntry>().FirstOrDefault(m => m.MediaId == id);
+        }
+
         public void AddMediaEntry(IMediaEntry mediaEntry)
         {
             if (!_mediaEntries.Contains(mediaEntry))
@@ -19,21 +26,43 @@ namespace MediaRatings.Domain.services
             }
         }
 
-        public void RemoveMediaEntry(IMediaEntry mediaEntry)
+        public bool RemoveMediaEntry(int mediaEntryId)
         {
-            if (_mediaEntries.Contains(mediaEntry))
-            {
-                _mediaEntries.Remove(mediaEntry);
-            }
+            var entry = GetMediaById(mediaEntryId);
+            if (entry == null) return false;
+
+            _mediaEntries.Remove(entry);
+            return true;
         }
 
-        public void UpdateMediaEntry(IMediaEntry oldEntry, IMediaEntry newEntry)
+        public bool UpdateMediaEntry(int oldMediaEntry, UpdateMediaDto mediaDto)
         {
-            int index = _mediaEntries.IndexOf(oldEntry);
-            if (index >= 0)
+            var entry = _mediaEntries.OfType<MediaEntry>().FirstOrDefault(m => m.MediaId == oldMediaEntry);
+            if (entry == null)
             {
-                _mediaEntries[index] = newEntry;
+                return false;
             }
+
+            Genres ParseGenre(string g) => g.ToLower() switch
+            {
+                "action" => Genres.Action,
+                "thriller" => Genres.Thriller,
+                "sci-fi" or "scifi" => Genres.SciFi,
+                "animation" => Genres.Animation,
+                "comedy" => Genres.Comedy,
+                "drama" => Genres.Drama,
+                "fantasy" => Genres.Fantasy,
+                "adventure" => Genres.Adventure,
+                _ => Genres.Unknown
+            };
+
+            entry.Title = mediaDto.Title;
+            entry.Description = mediaDto.Description;
+            entry.ReleaseYear = mediaDto.ReleaseYear;
+            entry.AgeRestriction = mediaDto.AgeRestriction;
+            entry.Genres = mediaDto.Genres.Select(ParseGenre).ToList();
+
+            return true;
         }
 
         public int CountMediaEntries()
