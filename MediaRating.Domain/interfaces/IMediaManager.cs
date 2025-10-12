d27bf7651e105878881689356d922c1684220226
Aaron Domingo <daaronjhay@gmx.at>
2025-10-12 10:52:19 +0200

    changed parameters from type IMediaEntry to type int and UpdateMediaDto

--- MediaRating.Domain/interfaces/IMediaManager.cs
+++ MediaRating.Domain/interfaces/IMediaManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Media_Ratings_Platform.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,8 @@ namespace MediaRatings.Domain.interfaces
     public interface IMediaManager
     {
         void AddMediaEntry(IMediaEntry mediaEntry);
-        void RemoveMediaEntry(IMediaEntry mediaEntry);
-        void UpdateMediaEntry(IMediaEntry oldEntry, IMediaEntry newEntry);
+        bool RemoveMediaEntry(int mediaEntryId);
+        bool UpdateMediaEntry(int oldMediaEntryId, UpdateMediaDto mediaDto);
         int CountMediaEntries();
         IReadOnlyCollection<IMediaEntry> GetAllMediaEntries();
     }
