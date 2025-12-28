2c6280a5b49c99099351aeb2356ac56f48ed70b7
Aaron Domingo <daaronjhay@gmx.at>
2025-12-28 11:00:09 +0100

    created new interface to handle dependency problem

---
+++ MediaRating.Domain/interfaces/IMediaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaRatings.Domain.interfaces
+{
+    public interface IMediaRepository
+    {
+        int CreateMediaEntry(IMediaEntry media);
+        IMediaEntry? GetMediaById(int mediaId);
+        IReadOnlyCollection<IMediaEntry> GetAllMedia();
+        bool UpdateMediaEntry(IMediaEntry media);
+        bool DeleteMediaEntry(int mediaId);
+    }
+}
