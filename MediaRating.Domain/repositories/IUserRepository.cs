e7e99ae9404621d30e40029ea7e86dde156b2a44
Aaron Domingo <daaronjhay@gmx.at>
2025-12-29 13:18:57 +0100

    added new method GetByIdAsync()

--- MediaRating.Domain/repositories/IUserRepository.cs
+++ MediaRating.Domain/repositories/IUserRepository.cs
@@ -9,6 +9,7 @@ namespace MediaRatings.Domain.repositories
     public interface IUserRepository
     {
         Task<UserAccount?> FindByUsernameAsync(string username);
+        Task<UserAccount?> GetByIdAsync(int userId);
         Task<UserAccount> CreateAsync(string username, string passwordPlain);
         Task<bool> ValidateCredentialsAsync(string username, string passwordPlain);
     }
