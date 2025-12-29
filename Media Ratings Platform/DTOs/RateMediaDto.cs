c0219ac131f6d7754c6862f3bb62029d1153f36d
Aaron Domingo <daaronjhay@gmx.at>
2025-12-29 13:15:34 +0100

    created

---
+++ Media Ratings Platform/DTOs/RateMediaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.DTOs
+{
+    public class RateMediaDto
+    {
+        public int Stars { get; set; }
+        public string? Comment { get; set; }
+    }
+}
