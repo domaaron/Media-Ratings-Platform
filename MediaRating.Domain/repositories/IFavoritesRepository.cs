19d708c4a630da2eb2da0ee59f09b0cb5e094b53
Aaron Domingo <daaronjhay@gmx.at>
2025-12-29 13:18:40 +0100

    created

---
+++ MediaRating.Domain/repositories/IFavoritesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaRatings.Domain.repositories
+{
+    public interface IFavoritesRepository
+    {
+        Task AddFavoriteAsync(int userId, int mediaId);
+        Task RemoveFavoriteAsync(int userId, int mediaId);
+        Task<IReadOnlyCollection<IMediaEntry>> GetFavoritesByUserAsync(int userId);
+        Task<bool> IsFavoritesAsync(int userId, int mediaId);
+    }
+}
---
+++ MediaRating.Domain/repositories/IRatingRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaRatings.Domain.repositories
+{
+    public interface IRatingRepository
+    {
+        Task<int> AddRatingAsync(UserRating rating);
+        Task UpdateRatingAsync(UserRating rating);
+        Task DeleteRatingAsync(int ratingId);
+        Task<UserRating?> GetRatingByIdAsync(int ratingId);
+        Task<IReadOnlyCollection<UserRating>> GetRatingByUserAsync(int userId);
+        Task<IReadOnlyCollection<UserRating>> GetRatingByMediaAsync(int userId);
+    }
+}
