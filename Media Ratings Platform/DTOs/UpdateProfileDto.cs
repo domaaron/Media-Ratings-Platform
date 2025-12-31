4731cbba25c7fb1ce132eeeed8408b7f9c3b6ef3
Aaron Domingo <daaronjhay@gmx.at>
2025-12-31 11:22:08 +0100

    created

---
+++ Media Ratings Platform/DTOs/UpdateProfileDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Ratings_Platform.DTOs
+{
+    public class UpdateProfileDto
+    {
+        public string? Email { get; set; }
+        public string? FavoriteGenre { get; set; }
+    }
+}
